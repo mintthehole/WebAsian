f8eaa44ce4c502305dd549ed43eb36c43709334e
mintthehole <johnpollo88@gmail.com>
2013-05-25 04:05:12 +0530

    Changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,44 @@
 class HomeController < ApplicationController
 	def index
 		unless current_user
-			redirect_to '/users/sign_in'
+			redirect_to '/sign_in'
 		end
 	end
+
+	def request_new_page
+	end
+
+  def real_time
+  end
+
+  def traffic_source
+  end
+
+  def seo
+  end
+
+  def social
+  end
+
+  def page_statistics
+  end
+
+  def request_new_page
+  end
+
+  def update_page
+  end
+
+  def delete_page
+  end
+
+  def change_plan
+  end
+
+  def create_form
+  end
+
+  def add_feed
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TestApp::Application.routes.draw do
-
-  devise_for :users
-
+ devise_for :users do
+   get "/sign_in", :to => "devise/sessions#new"
+ end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,10 +52,12 @@ TestApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "home#index"
+  match '/index' => 'home#index'
+
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match '(/:action(/:id(.:format)))' => 'home#:action'
 end
