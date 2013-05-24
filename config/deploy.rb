1c76fef2f4e0dac45a603d41af76a24b1ce01dc0
mintthehole <johnpollo88@gmail.com>
2013-05-24 20:31:35 +0530

    Changes

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+require 'bundler/capistrano'
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+
+set :application, "test_app"
+set :repository,  "git@github.com:mintthehole/WebAsian.git"
+set :scm, :git
+
+set :scm_username, 'mintthehole
+
+
+
+
+
+
+
+
+
+
+
+
+'
+
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :use_sudo, false
+
+def aws_staging name
+	task name do
+		yield    
+		role :app, domain
+		role :web, domain
+		role :db, domain, :primary => true
+		ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+	end
+end
+
+aws_staging :ec2_staging do
+	set :user, 'sunbeemmaxp2011'  # Your dreamhost account's username
+	set :domain, 'manage.webasiansystems.com'  # Dreamhost servername where your account is located 
+	set :project, 'test_app'  # Your application as its called in the repository
+	set :application, 'manage.webasiansystems.com'  # Your app's location (domain or sub-domain name as setup in panel)
+	set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
+	set :deploy_to, applicationdir
+	set :branch, "master"
+end
+
+
+
+namespace :deploy do
+	task :start do ; end
+	task :stop do ; end
+
+	task :restart, :roles => :app, :except => { :no_release => true } do
+		run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+	end
+end
